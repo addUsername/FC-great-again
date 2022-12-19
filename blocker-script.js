9f9bce52a2da12cc15b6414d1fad153806eb5649
addUsername <60299373+addUsername@users.noreply.github.com>
2022-12-19 22:45:57 +0100

    Initial commit

---
+++ blocker-script.js
@@ -0,0 +1,30 @@
+let mode = 0
+applyChanges()
+
+chrome.runtime.onMessage.addListener(function (response, sendResponse) {
+    mode = (mode + 1) % 3
+    applyChanges()
+    return true
+});
+
+
+function applyChanges(){
+    let body = document.getElementById("threadbits_forum_8")
+    if(mode !== 2){
+        for (let idx = 0; idx < body.children.length; idx++) {
+            const title = body.children[idx].children[2].getAttribute("title")
+            if(title.match(/twitter|tiktok|instagram/g)){
+                if(mode === 0){
+                    body.children[idx].children[2].children[0].children[0].innerHTML = "🤢🤢 <s>" +body.children[idx].children[2].children[0].children[0].innerHTML + "</s>"
+                }
+                if(mode === 1){
+                    body.children[idx].remove()
+                }
+            }
+        }
+    }else{
+        window.location.reload()
+    }
+}
+
+
