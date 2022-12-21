f6902ac58886b3f516a868b4b3f660cac58a6b6c
addUsername <60299373+addUsername@users.noreply.github.com>
2022-12-21 20:59:35 +0100

    Add request blocker

--- blocker-script.js
+++ blocker-script.js
@@ -1,30 +1,41 @@
-let mode = 0
-applyChanges()
-
-chrome.runtime.onMessage.addListener(function (response, sendResponse) {
-    mode = (mode + 1) % 3
-    applyChanges()
-    return true
+chrome.runtime.onMessage.addListener((response, sendResponse) => {
+  applyChanges();
 });
 
+function applyChanges() {
+  chrome.storage.sync.get("mode", function (m) {
+    mode = m.mode ?? 0;
+    if (window.location.href.includes("forumdisplay")) {
+      if (mode === 2) {
+        window.location.reload();
+        return;
+      }
 
-function applyChanges(){
-    let body = document.getElementById("threadbits_forum_2")
-    if(mode !== 2){
-        for (let idx = 0; idx < body.children.length; idx++) {
-            const title = body.children[idx].children[2].getAttribute("title")
-            if(title.match(/twitter|tiktok|instagram/g)){
-                if(mode === 0){
-                    body.children[idx].children[2].children[0].children[0].innerHTML = "🤢🤢 <s>" +body.children[idx].children[2].children[0].children[0].innerHTML + "</s>"
-                }
-                if(mode === 1){
-                    body.children[idx].remove()
-                }
+      let body = document.getElementById("threadbits_forum_2");
+      for (let idx = 0; idx < body.children.length; idx++) {
+        const title = body.children[idx].children[2].getAttribute("title");
+        if (title.match(/twitter|tiktok|instagram/g)) {
+          //improve to censor just the urls
+          if (mode === 0) {
+            const post = body.children[idx].children[2].children[0];
+
+            if (post.children.length > 1) {
+              post.children[1].innerHTML =
+                "🤢🤢 <s>" + post.children[1].innerHTML + "</s>";
+            } else {
+              post.children[0].innerHTML =
+                "🤢🤢 <s>" + post.children[0].innerHTML + "</s>";
             }
+          }
+          if (mode === 1) {
+            body.children[idx].remove();
+          }
         }
-    }else{
-        window.location.reload()
+      }
+    } else {
+      if (mode !== 0) {
+        window.location.reload();
+      }
     }
+  });
 }
-
-
