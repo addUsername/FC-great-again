8f5ee2e3092af6a241b67f4ef966677261bfd1fd
addUsername <60299373+addUsername@users.noreply.github.com>
2022-12-22 22:06:39 +0100

    Add block rule for mode == 1

---
+++ background.js
@@ -0,0 +1,66 @@
+const ruleID = 2;
+const domains = ["twitter.com", "tiktok.com", "instagram.com"];
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({ text: "!!" });
+  chrome.action.setBadgeBackgroundColor({ color: "#FFA500" });
+
+  chrome.storage.sync.set({ mode: -1 }, () => {
+    //console.log("Installed")
+  });
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "mode") {
+    chrome.storage.sync.get("mode", (m) => {
+      let mode = m.mode ?? 0;
+      mode = (mode + 1) % 3;
+
+      //console.log("change mode: ", mode);
+
+      if (mode === 1) {
+        block(domains);
+      } else {
+        unblock();
+      }
+      chrome.action.setBadgeText({ text: "" + mode });
+
+      chrome.storage.sync.set({ mode: mode }, () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.tabs.sendMessage(tabs[0].id, mode);
+        });
+      });
+    });
+  }
+});
+
+function block() {
+  chrome.declarativeNetRequest.updateDynamicRules(
+    {
+      addRules: [
+        {
+          action: {
+            type: "block",
+          },
+          condition: {
+            urlFilter: "*",
+            domains: domains,
+          },
+          id: ruleID,
+          priority: 1,
+        },
+      ],
+      removeRuleIds: [ruleID],
+    },
+    () => {
+      //console.log("block rule added for", domains);
+    }
+  );
+}
+
+function unblock() {
+  chrome.declarativeNetRequest.updateDynamicRules(
+    { removeRuleIds: [ruleID] },
+    () => //console.log("rule removed")
+  );
+}
---
+++ content.js
@@ -0,0 +1,34 @@
+chrome.runtime.onMessage.addListener((response, sendResponse) => {
+  applyChanges(response);
+});
+
+function applyChanges(mode) {
+  if (window.location.href.includes("forumdisplay")) {
+    if (mode === 2) {
+      window.location.reload();
+      return;
+    }
+
+    let body = document.getElementById("threadbits_forum_2");
+    for (let idx = 0; idx < body.children.length; idx++) {
+      const title = body.children[idx].children[2].getAttribute("title");
+      
+      if (title.match(/twitter|tiktok|instagram/g)) {
+        if (mode === 0) {
+          const post = body.children[idx].children[2].children[0];
+          const index = post.children.length > 1 ? 1 : 0;
+          post.children[index].innerHTML =
+            "🤢🤢 <s>" + post.children[index].innerHTML + "</s>";
+        }
+
+        if (mode === 1) {
+          body.children[idx].remove();
+        }
+      }
+    }
+  } else {
+    if (mode !== 0) {
+      window.location.reload();
+    }
+  }
+}
